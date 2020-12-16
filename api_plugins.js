66f52a9f5edcc8bfdd453ddd6f4cbb18070fbfe0
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-12-16 14:51:58 +0300

    Added new plugins methods for add content control

--- api_plugins.js
+++ api_plugins.js
@@ -72,4 +72,108 @@
 		return {"Tag" : _obj.Tag, "Id" : _obj.Id, "Lock" : _obj.Lock, "InternalId" : _obj.InternalId};
 	};
 
+	/**
+	 * This method allows to add an empty content control checkbox to the document.
+	 * @memberof Api
+	 * @typeofeditors ["CDE"]
+	 * @alias AddContentControlCheckBox
+	 * @param {ContentControlCkeckBoxProperties}  [ properties = {} ] is property of content control checkbox
+	 * @example
+	 * var properties = {"Checked": false, "CheckedSymbol": 9746, "UncheckedSymbol": 9744};
+	 * window.Asc.plugin.executeMethod("AddContentControlCheckBox", [properties]);
+	 */
+	window["asc_docs_api"].prototype["pluginMethod_AddContentControlCheckBox"] = function(properties)
+	{
+		var oPr;
+		if (properties)
+		{
+			oPr = new AscCommon.CSdtCheckBoxPr()
+			if (properties["Checked"])
+				oPr.SetChecked(properties["Checked"]);
+			if (properties["CheckedSymbol"])
+				oPr.SetCheckedSymbol(properties["CheckedSymbol"]);
+			if (properties["UncheckedSymbol"])
+				oPr.SetUncheckedSymbol(properties["UncheckedSymbol"]);
+		}
+		this.asc_AddContentControlCheckBox(oPr);
+	};
+
+	/**
+	 * This method allows to add an empty content control picture to the document.
+	 * @memberof Api
+	 * @typeofeditors ["CDE"]
+	 * @alias AddContentControlPicture
+	 * @example
+	 * window.Asc.plugin.executeMethod("AddContentControlPicture");
+	 */
+	window["asc_docs_api"].prototype["pluginMethod_AddContentControlPicture"] = function()
+	{
+		this.asc_AddContentControlPicture();
+	};
+
+	/**
+	 * This method allows to add an empty content control list to the document.
+	 * @memberof Api
+	 * @typeofeditors ["CDE"]
+	 * @alias AddContentControlList
+	 * @param {ContentControlType} type is a numeric value that specifies the content control type
+	 * @param {Array[{String, String}]}  [ List = [{sDisplay, sValue}] ] is property of content control List
+	 * @example
+	 * var type = 1; //1 - ComboBox  0 - DropDownList
+	 * var List = [{sDisplay: "Item1_D", sValue: "Item1_V"}, {sDisplay: "Item2_D", sValue: "Item2_V"}];
+	 * window.Asc.plugin.executeMethod("AddContentControlList", [type, List]);
+	 */
+	window["asc_docs_api"].prototype["pluginMethod_AddContentControlList"] = function(type, List)
+	{
+		var oPr;
+		if (List)
+		{
+			oPr = new AscCommon.CSdtComboBoxPr();
+			List.forEach(function(el) {
+				oPr.AddItem(el.sDisplay, el.sValue);
+			});
+		}
+		this.asc_AddContentControlList(type, oPr);
+	};
+
+	/**
+	 * This method allows to add an empty content control datepicker to the document.
+	 * @memberof Api
+	 * @typeofeditors ["CDE"]
+	 * @alias AddContentControlDatePicker
+	 * @param {ContentControlDatePickerProperties}  [ properties = {} ] is property of content control datepicker
+	 * @example
+	 * var DateFormats = [
+	 * "MM/DD/YYYY",
+	 * "dddd\,\ mmmm\ dd\,\ yyyy",
+	 * "DD\ MMMM\ YYYY",
+	 * "MMMM\ DD\,\ YYYY",
+	 * "DD-MMM-YY",
+	 * "MMMM\ YY",
+	 * "MMM-YY",
+	 * "MM/DD/YYYY\ hh:mm\ AM/PM",
+	 * "MM/DD/YYYY\ hh:mm:ss\ AM/PM",
+	 * "hh:mm",
+	 * "hh:mm:ss",
+	 * "hh:mm\ AM/PM",
+	 * "hh:mm:ss:\ AM/PM"
+	 * ];
+	 * var Date = new window.Date();
+	 * var properties = {"DateFormat" : DateFormats[2], "Date" : Date};
+	 * window.Asc.plugin.executeMethod("AddContentControlDatePicker", [properties]);
+	 */
+	window["asc_docs_api"].prototype["pluginMethod_AddContentControlDatePicker"] = function(properties)
+	{
+		var oPr;
+		if (properties)
+		{
+			oPr = new AscCommon.CSdtDatePickerPr();
+			if (properties.Date)
+				oPr.SetFullDate(properties.Date);
+			if (properties.DateFormat)
+				oPr.SetDateFormat(properties.DateFormat);
+		}
+		this.asc_AddContentControlDatePicker(oPr);
+	};
+
 })(window);
